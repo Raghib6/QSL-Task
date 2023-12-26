8e288469ec02642658f4a14f258cd405db285002
Raghib6 <raghibshahriar6@gmail.com>
2023-12-26 15:29:50 +0600

    Added models and configured media and static urls

--- store_app/admin.py
+++ store_app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from store_app.models import Brand, Seller, Category, Warranty, Product
 
-# Register your models here.
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "created_at"]
+    search_fields = ["name"]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "created_at"]
+    search_fields = ["name"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "parent", "created_at"]
+    search_fields = ["name"]
+
+
+@admin.register(Warranty)
+class WarrantyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "created_at"]
+    search_fields = ["name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_filter = ["category"]
+    search_fields = ["name", "category__name"]
+    list_display = ["name", "brand", "category", "price", "seller", "warranty"]
--- store_app/models.py
+++ store_app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Seller(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="child"
+    )
+    category_image = models.ImageField(upload_to="categories")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Warranty(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "warranty"
+        verbose_name_plural = "warranties"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=200, unique=True)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="all_products", on_delete=models.CASCADE
+    )
+    warranty = models.ForeignKey(
+        Warranty, related_name="_products", on_delete=models.CASCADE
+    )
+    price = models.IntegerField()
+    product_image = models.ImageField(upload_to="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
